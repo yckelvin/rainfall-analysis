2f7c3baccd118274fa9537c36760cfd5371e6a97
Kelvin Lee <yckelvin@users.noreply.github.com>
2024-04-22 15:29:20 +0800

    add forever and reset OLED

--- main.ts
+++ main.ts
@@ -1,37 +1,10 @@
 input.onButtonPressed(Button.A, function () {
+    if (count > 5) {
+        microIoT.microIoT_clear()
+    }
     Rainfall = pins.analogReadPin(AnalogPin.P1)
     microIoT.microIoT_showUserText(count, "Rainfall: " + ("" + Rainfall))
     count += 1
-    if (Rainfall > 250) {
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-        microIoT.microIoT_setIndexColor(PIN.P15, 0, 2, 0x65471f)
-    } else if (Rainfall > 270) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-        microIoT.microIoT_setIndexColor(PIN.P15, 0, 2, 0xff0000)
-    } else if (Rainfall > 0) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            # # # # #
-            `)
-        microIoT.microIoT_setIndexColor(PIN.P15, 0, 2, 0xffff00)
-    } else {
-        microIoT.microIoT_ledBlank(PIN.P15)
-    }
 })
 input.onButtonPressed(Button.AB, function () {
     control.reset()
@@ -53,3 +26,35 @@ microIoT.microIoT_showUserText(0, "Ready!")
 basic.showNumber(2)
 microIoT.microIoT_ledBlank(PIN.P15)
 basic.showNumber(3)
+basic.forever(function () {
+    if (Rainfall > 300) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        microIoT.microIoT_setIndexColor(PIN.P15, 0, 2, 0x65471f)
+    } else if (Rainfall > 200) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        microIoT.microIoT_setIndexColor(PIN.P15, 0, 2, 0xff0000)
+    } else if (Rainfall > 100) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+        microIoT.microIoT_setIndexColor(PIN.P15, 0, 2, 0xffff00)
+    } else {
+        microIoT.microIoT_ledBlank(PIN.P15)
+    }
+})
